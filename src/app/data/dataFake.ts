428a4023207d8e60615067d9536d2f1928cc3a66
Bowie <marketing@letscode.com.br>
2024-01-27 23:06:44 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+export const dataFake = [
+	{
+		"id":'1',
+		"photo": "link",
+		"title": "Black Panther",
+		"description": "Conheça a história de T'Challa, príncipe do reino de Wakanda, que perde o seu pai e viaja para os Estados Unidos, onde tem contato com os Vingadores. Entre as suas habilidades estão a velocidade, inteligência e os sentidos apurados.",
+		"photoCover": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgZYkNum5Y6J6H9YH3Ur1cATNvjJzYkYq90DZ1KUW0KSmZB0BGn_R5wIYQH8aPKCrFUzAh95n0Lku9Opl3PNUhZ4ErpNRnsBJyQ3z1KsEk40MFqDsGthhwLTOmKa2LrzJeGsmGTE8ZKh7ON8VZyvqgfsMIsLOR-g0thN7QmIm0bG5wW6iC-ttZi1pS_/s1200/balaio-de-babados-protagonistas-pantera-negra-wakanda-para-sempre-poster.jpg"
+	},
+
+	{
+		"id":'2',
+		"photo": "link",
+		"title": "THOR LOVE AND THUNDER",
+		"description": "Thor busca por paz, mas seus planos são interrompidos por Gorr, o Carniceiro dos Deuses, um assassino galáctico. Para combater esta ameaça, ele pede que Rei Valquíria, Korg e Jane Foster o ajudem.",
+		"photoCover": "https://img.olhardigital.com.br/wp-content/uploads/2022/06/thor-reacoes.jpg"
+
+	},
+
+	{
+		"id":'3',
+		"photo": "link",
+		"title": "LOKI",
+		"description": "Loki, Deus da Trapaça, sai da sombra de seu irmão para embarcar em uma aventura que ocorre após os eventos de Vingadores: Ultimato.",
+		"photoCover": "https://1.bp.blogspot.com/-FGss-ZwVTbY/YKfyx6A0W6I/AAAAAAACMfU/Mslo3Fg06-0ptVplxfJ-twevVLC6mA5swCLcBGAsYHQ/s540/ewloki.jpg"
+
+	},
+
+	{
+		"id":'4',
+		"photo": "link",
+		"title": "Guardiões da galáxia",
+		"description": "O aventureiro do espaço Peter Quill torna-se presa de caçadores de recompensas depois que rouba a esfera de um vilão traiçoeiro, Ronan. Para escapar do perigo, ele faz uma aliança com um grupo de quatro extraterrestres. Quando Quill descobre que a esfera roubada possui um poder capaz de mudar os rumos do universo, ele e seu grupo deverão proteger o objeto para salvar o futuro da galáxia.",
+		"photoCover": "https://t.ctcdn.com.br/NyrlOvNzu1NjvTAgOskGjI0GBDA=/81x0:1340x709/1259x708/smart/i335724.jpeg"
+
+	},
+]
---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+	photoCover:string = ''
+	contenttitle:string = ''
+	contentDescription:string = 'Thor busca por paz, mas seus planos são interrompidos por Gorr, o Carniceiro dos Deuses, um assassino galáctico. Para combater esta ameaça, ele pede que Rei Valquíria, Korg e Jane Foster o ajudem.'
+	private id:string | null = '0'
+
+  constructor(
+		private route:ActivatedRoute
+	) { }
+
+  ngOnInit(): void {
+		this.route.paramMap.subscribe(value =>
+			this.id = value.get('id')
+			)
+
+			this.setValuesToComponent(this.id)
+  }
+	setValuesToComponent(id:string | null){
+		const result = dataFake.filter(article => article.id == id)[0]
+
+		this.contentDescription = result.description
+		this.contenttitle = result.title
+		this.photoCover = result.photoCover
+
+	}
+
+}
